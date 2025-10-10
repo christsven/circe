59e98bb8e5549ce9e1be5277bdd875ba643b33f2
Sven <69843539+christsven@users.noreply.github.com>
2025-10-10 22:44:58 +0200

    Fix typo

--- lua/circe/groups.lua
+++ lua/circe/groups.lua
@@ -5,7 +5,7 @@ local colors = require("circe.palette")
 M.setup = function()
   return {
     Normal = { fg = colors.fg, bg = NONE },
-    NormalFloat = { fg = colors.fg, bg = color.bg },
+    NormalFloat = { fg = colors.fg, bg = colors.bg },
     Comment = { fg = colors.comment, italic = true },
     Constant = { fg = colors.pink },
     String = { fg = colors.number },
