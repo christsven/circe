0baff3f728fe22ddf150520222b6ec8e10eaf578
christsven <chrsve@gmx.de>
2025-11-06 19:02:40 +0100

    polish palette

--- lua/circe/palette.lua
+++ lua/circe/palette.lua
@@ -1,57 +1,64 @@
-basecolors = {
-  red = '#a64242',
-  orange = '#e26453',
-  yellow = '#f0c674',
-  green = '#9aa212',
-  blue = '#0074b3',
-  magenta = '#da667b',
-  gray = '#98b0a2',
-  white = '#f8e5c2',
-  fg = '#c9b79c',
-  bg = '#0c0101',
-  secondary = '#97a587',
-  disabled = ''
+base = {
+  red       = '#a64242',
+  orange    = '#e26453',
+  yellow    = '#f0c674',
+  green     = '#9aa212',
+  blue      = '#0074b3',
+  cyan      = '#97a587',
+  pink      = '#da667b',
+  gray      = '#98b0a2',
+  white     = '#ffffff',
+  fg        = '#f8e5c2',
+  secondary = '#c9b79c',
+  bg        = '#0c0101',
+  disabled  = ''
 }
 
 return {
 
---- colors
+--- direct base colors
 
-  red = basecolors.red,
-  orange = basecolors.orange,
-  green = basecolors.green,
-  yellow = basecolors.yellow,
-  blue = basecolors.blue,
-  pink = basecolors.pink,
-  gray = basecolors.gray,
-  white = basecolors.white,
-  fg = basecolors.fg,
-  bg = basecolors.bg,
-  secondary = basecolors.secondary,
+  red = base.red,
+  orange = base.orange,
+  green = base.green,
+  yellow = base.yellow,
+  blue = base.blue,
+  pink = base.pink,
+  cyan = base.cyan,
+  gray = base.gray,
+  white = base.white,
+  fg = base.fg,
+  bg = base.bg,
+  secondary = base.secondary,
 
---- components
+--- functionally bound colours for easier usage 
 
-  modified = basecolors.yellow,
-  branch = basecolors.yellow,
-  method = basecolors.yellow,
-  constant = basecolors.pink,
-  funct = basecolors.orange,
-  variable = basecolors.white,
-  keyword = basecolors.yellow,
-  operator = basecolors.yellow,
-  comment = basecolors.gray,
-  path = basecolors.secondary,
-  info = basecolors.fg,
-  hint = basecolors.secondary,
-  warning = basecolors.orange,
-  error = basecolors.red,
-  status = basecolors.secondary,
-  inactive= basecolors.gray,
-  number = basecolors.fg,
-  boolean = basecolors.yellow,
-  selection = basecolors.blue,
+  modified  = base.yellow,
+  branch    = base.yellow,
 
---- misc
+  method    = base.yellow,
+  funct     = base.orange,
 
-  cursor = basecolors.fg,
+  constant  = base.pink,
+  keyword   = base.orange,
+  operator  = base.yellow,
+  path      = base.secondary,
+
+  variable  = base.secondary,
+  number    = base.secondary,
+  boolean   = base.secondary,
+
+  info      = base.white,
+  hint      = base.green,
+  warning   = base.orange,
+  error     = base.red,
+  status    = base.secondary,
+
+  comment   = base.gray,
+  inactive  = base.gray,
+  selection = base.blue,
+
+--- functional and UI 
+
+  cursor = base.fg,
 }
